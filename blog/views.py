e754066b4945f3e768457a7865d64ebbc0fd35ca
val-sytch <val.sytch@gmail.com>
2016-10-09 17:26:38 +0300

    add some style, detail page, datetime style performance updated

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    detail = Post.objects.filter(pk=pk).first()
+    return render(request, 'blog/single.html', {'detail': detail})
